da5dea5eab723f6aeb9f173ba5bd1ac677567af3
Mohamed Ashraf <mohamedashraff.hussein@gmail.com>
2024-09-09 18:02:33 +0300

    enhanced UI & added firestore

--- TODO Task/index.js
+++ TODO Task/index.js
@@ -1,49 +1,49 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAfuuqSFlrS4Rhs_tnzk4J1AnkXe9XvoOI",
+  authDomain: "todo-825ca.firebaseapp.com",
+  projectId: "todo-825ca",
+  storageBucket: "todo-825ca.appspot.com",
+  messagingSenderId: "233213895048",
+  appId: "1:233213895048:web:5b684e04aaa58e976f1a3d",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
 const tasksArray = [];
 const tasksCompleted = [];
 
-function generateId() {
-  return Math.random().toString(36);
-}
+const taskForm = document.getElementById("task-form");
 
-document.getElementById("task-form").addEventListener("submit", function (e) {
+taskForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const taskName = document.getElementById("task-name").value;
   const taskDescription = document.getElementById("task-description").value;
   const taskPriority = document.getElementById("task-priority").value;
 
-  if (taskName === "" || taskDescription === "" || taskPriority === "") {
-    alert("Please fill out all fields");
+  if (!taskName || !taskDescription || taskPriority === "") {
+    alert("Please fill out all fields.");
     return;
   }
 
-  const task = {
-    id: generateId(),
-    name: taskName,
-    description: taskDescription,
-    priority: taskPriority,
-    completed: false,
-  };
-
-  addTask(task);
-  renderTasks();
-  clearForm();
+  try {
+    const docRef = await db.collection("todos").add({
+      name: taskName,
+      description: taskDescription,
+      priority: taskPriority,
+      completed: false,
+    });
+    console.log("Task added with ID: ", docRef.id);
+    renderTasks();
+    taskForm.reset();
+    location.reload();
+  } catch (error) {
+    console.error("Error adding task: ", error);
+  }
 });
 
-function addTask(task) {
-  tasksArray.push(task);
-  tasksArray.sort((a, b) => a.priority - b.priority);
-}
-
-function completeTask(id) {
-  const taskIndex = tasksArray.findIndex((task) => task.id === id);
-  if (taskIndex !== -1) {
-    tasksArray[taskIndex].completed = true;
-    tasksCompleted.push(tasksArray.splice(taskIndex, 1)[0]);
-    renderTasks();
-  }
-}
-
+// Rendering tasks
 function renderTasks(
   filteredTasks = tasksArray,
   filteredCompletedTasks = tasksCompleted
@@ -53,36 +53,69 @@ function renderTasks(
 
   filteredTasks.forEach((task) => {
     const taskElement = document.createElement("li");
-    taskElement.classList.add("list-group-item");
+    taskElement.classList.add(
+      "list-group-item",
+      "d-flex",
+      "d-flex",
+      "justify-content-between"
+    );
     taskElement.innerHTML = `
-        <h3>${task.name}</h3>
-        <p>${task.description}</p>
-        <p>Priority: ${task.priority}</p>
-        <p class="badge bg-warning">Pending</p>
-        <button class="btn btn-success" onclick="completeTask('${task.id}')">Complete</button>
-      `;
+      <h4>${task.name}</h4>
+      <p>${task.description}</p>
+      <p>Priority: ${task.priority}</p>
+      <button class="btn btn-success" onclick="completeTask('${task.id}')">Complete</button>
+      <p class="badge bg-warning">Pending</p>
+    `;
     tasksList.appendChild(taskElement);
   });
 
-  // Render completed tasks at the end
   filteredCompletedTasks.forEach((task) => {
     const taskElement = document.createElement("li");
-    taskElement.classList.add("list-group-item", "bg-light", "text-muted");
+    taskElement.classList.add(
+      "list-group-item",
+      "bg-light",
+      "text-muted",
+      "d-flex",
+      "justify-content-between"
+    );
     taskElement.innerHTML = `
-        <h3>${task.name}</h3>
-        <p>${task.description}</p>
-        <p>Priority: ${task.priority}</p>
-        <p class="badge bg-success">Completed</p>
-      `;
+      <h4>${task.name}</h4>
+      <p>${task.description}</p>
+      <p>Priority: ${task.priority}</p>
+      <p class="badge bg-success">Completed</p>
+    `;
     tasksList.appendChild(taskElement);
   });
 }
 
-function clearForm() {
-  document.getElementById("task-name").value = "";
-  document.getElementById("task-description").value = "";
-  document.getElementById("task-priority").value = "0";
+function completeTask(taskId) {
+  try {
+    db.collection("todos").doc(taskId).update({
+      completed: true,
+    });
+    const taskIndex = tasksArray.findIndex((task) => task.id === taskId);
+    const task = tasksArray.splice(taskIndex, 1);
+    tasksCompleted.push(task[0]);
+    renderTasks();
+  } catch (error) {
+    console.error("Error completing task: ", error);
+  }
 }
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const querySnapshot = await db.collection("todos").get();
+    querySnapshot.forEach((doc) => {
+      if (doc.data().completed) {
+        tasksCompleted.push({ id: doc.id, ...doc.data() });
+        return;
+      } else tasksArray.push({ id: doc.id, ...doc.data() });
+    });
+    renderTasks(tasksArray);
+  } catch (error) {
+    console.error("Error fetching tasks: ", error);
+  }
+});
 
 document.getElementById("search-input").addEventListener("input", function (e) {
   const searchTerm = e.target.value.toLowerCase();
